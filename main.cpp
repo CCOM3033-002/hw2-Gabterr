f9930d083bc304322777daafdbf197ab0e967279
Gabterr <gabriel.ramos42@upr.edu>
2025-02-13 01:48:48 +0000

    adding fractions code

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
-// escriba su nombre, username en github y numero de estudiante en el header
-// incluya cualquier colaboración que haga
+/* 
+Gabriel A. Ramos Rivera
+Gabterr
+801-24-0582
+*/
 #include<iostream>
 using namespace std;
 
 int main(){
-  // escriba su código aquí 
-  
+  // Despliega el proposito del programa
+  cout << "Este programa suma dos fracciones." << endl;
+
+  // Pedir numerador y denominador de ambas fracciones. 
+  double nume1, deno1, nume2, deno2;
+  cout << "Entre el numerador de la 1ra fracción: ";
+  cin >> nume1;
+  cout << "Entre el denominador de la 1ra fracción: ";
+  cin >> deno1;
+  cout << "Entre el numerador de la 2da fracción: ";
+  cin >> nume2;
+  cout << "Entre el denominador de la 2da fracción: ";
+  cin >> deno2;
+
+  // Calcular la suma de las fracciones 
+  double nume_result = nume1 * deno2 + deno1 * nume2, deno_result = deno1 * deno2;
+
+  // Despliegar resultado
+  cout << "La suma es: " << nume_result << "/" << deno_result << endl;
+
   return 0;
 }
